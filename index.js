c38d8d6edeea16a546e52f133c17bd11176d4dc9
user <juyelr16@gmail.com>
2022-05-27 16:00:15 +0900

    new api

--- index.js
+++ index.js
@@ -88,21 +88,34 @@ async function run() {
         })
 
 
-        //New user Add
-        app.put('/user/admin/:email', async (req, res) => {
+        //New user admin Add
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                const filter = { email: email };
 
-            const filter = { email: email };
-
-            const updateDoc = {
-                $set: { role: 'admin' },
-            };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.send(result);
+                const updateDoc = {
+                    $set: { role: 'admin' },
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
 
         });
 
+        //add a only admin
 
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+        })
 
 
 
